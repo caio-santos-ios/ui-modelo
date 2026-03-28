3132892bb60d197319957148b949245d1bdc785d
Caio Santos <caiosantos.dev@outlook.com>
2026-03-28 16:26:38 -0300

    feat: add url production

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const url = process.env.NEXT_PUBLIC_API_URL;
+console.log("API Service initialized with base URL:", url);
 export const uriBase = "http://localhost:5097";
 export const baseURL = `${uriBase}/api`;
 
