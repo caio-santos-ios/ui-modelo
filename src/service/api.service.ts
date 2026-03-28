f9d9d0175e0bc492c8fe80f20c144d8d97ea9ad8
Caio Santos <caiosantos.dev@outlook.com>
2026-03-28 18:51:19 -0300

    fix

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const url = process.env.NEXT_PUBLIC_API_URL;
-console.log("API Service initialized with base URL:", url);
 export const uriBase = url;
 export const baseURL = `${uriBase}/api`;
 
