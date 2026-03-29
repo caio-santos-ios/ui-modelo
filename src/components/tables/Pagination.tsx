9bdb9da36c89d8ad5c3ec770f30af9e979590289
Caio Santos <caiosantos.dev@outlook.com>
2026-03-29 15:04:00 -0300

    fix

--- src/components/tables/Pagination.tsx
+++ src/components/tables/Pagination.tsx
@@ -40,12 +40,12 @@ const Pagination = ({currentPage, totalData, totalPages, totalCount, onPageChang
         <button
           onClick={() => onPageChange(currentPage - 1)}
           disabled={currentPage === 1}
-          className="mr-2.5 flex items-center h-10 justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs hover:bg-gray-50 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 text-sm"
+          className="mr-2 flex items-center h-10 justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs hover:bg-gray-50 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 text-sm"
         >
           Anterior
         </button>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1">
           {getPageNumbers().map((page, index) =>
             page === "..." ? (
               <span key={`ellipsis-${index}`} className="px-2 text-gray-500 dark:text-gray-400">
@@ -70,7 +70,7 @@ const Pagination = ({currentPage, totalData, totalPages, totalCount, onPageChang
         <button
           onClick={() => onPageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
-          className="ml-2.5 flex items-center justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs text-sm hover:bg-gray-50 h-10 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3"
+          className="ml-2 flex items-center justify-center rounded-lg border border-gray-300 bg-white px-3.5 py-2.5 text-gray-700 shadow-theme-xs text-sm hover:bg-gray-50 h-10 disabled:opacity-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3"
         >
           Próximo
         </button>
